6d3a513c93b4f6c289040d63af857f7b1ae8061f
igmrsky <mrinskiy0282@mail.ru>
2024-05-01 21:25:44 +0400

    added full site no-adaptive version

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
-$(function(){
+$(function () {
 
+    $('.faq__caption').on('click', function () {
+        $(this).next('.faq__wrapper').slideToggle();
+        $(this).toggleClass('faq__caption--active');
+    });
+
+    $('.team__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+    });
 
 });
